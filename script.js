ad6a978d59288ac8197cb5456d5ae1593e9e8feb
David Lei <DavidtheDefender2003@gmail.com>
2022-07-30 18:11:54 -0700

    Add function to check if character is letter in English alphabet

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const wordTarget = document.querySelector(".word-target-entry");
 const textField = document.querySelector(".text-entry");
 const wordDisplay = document.querySelector(".num-words-display");
+
+function isLetter(character) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    
+    return alphabet.includes(character.toLowerCase());
+}
