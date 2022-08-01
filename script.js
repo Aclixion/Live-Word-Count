81c4e079275dbf0228affac62f1ea7d6341d5a18
David Lei <DavidtheDefender2003@gmail.com>
2022-08-01 12:00:33 -0700

    Add comments to clarify code

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// Input for number of words to reach
 const wordTarget = document.querySelector(".word-target-entry");
+// Input for text
 const textField = document.querySelector(".text-entry");
+// Display for whether or not the target number of words has been reached
 const wordDisplay = document.querySelector(".num-words-display");
 
+// Checks whether or not a character is a letter from the English alphabet
 function isLetter(character) {
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
     
