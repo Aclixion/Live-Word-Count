be608fc4ce0ff31f6ffde68b8d0b72a7466f19d4
David Lei <DavidtheDefender2003@gmail.com>
2022-08-03 16:32:00 -0700

    Begin overhaul of events

--- script.js
+++ script.js
@@ -35,19 +35,3 @@ function numWords(string) {
     }
     return numWords;
 }
-
-function updateResults() {
-    // Number of words in text input
-    let numberWords = numWords(textField.value);
-    // Changes text color based on number of words
-    if (numberWords >= Number(wordTarget.value)) {
-        wordDisplay.style.color = "green";
-    } else {
-        wordDisplay.style.color = "red";
-    }
-    // Update display text
-    wordDisplay.textContent = `Number of words: ${numberWords}`;
-}
-
-textField.addEventListener("input", updateResults);
-wordTarget.addEventListener("input", updateResults);
